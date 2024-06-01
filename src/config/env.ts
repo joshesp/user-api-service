24d3095fd04d09b5fb7220b522c3d53c6ed0efde
Josh <josh@imac.com>
2024-06-01 15:24:17 -0600

    actualización de propiedades de environment

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || '3000';
-const PRODUCTION = (process.env.PRODUCTION?.toLowerCase() === 'true');
+const IS_PRODUCTION = (process.env.NODE_ENV !== 'development');
 
 const POSTGRES_CONNECTION = process.env.POSTGRES_CONNECTION;
 const POSTGRES_CONNECTION_TIMEOUT = process.env.POSTGRES_CONNECTION_TIMEOUT ?? '10000';
@@ -12,7 +12,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
 
 export {
-  JWT_EXPIRATION, JWT_SECRET, PORT,
-  POSTGRES_CONNECTION, POSTGRES_CONNECTION_TIMEOUT, PRODUCTION
+  IS_PRODUCTION, JWT_EXPIRATION, JWT_SECRET, PORT,
+  POSTGRES_CONNECTION, POSTGRES_CONNECTION_TIMEOUT
 };
 
