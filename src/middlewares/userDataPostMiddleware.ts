a52ce6a7191af5fffd756bc2b9f38cff27ca00ff
Josh <benjamin.espinosa.dev@gmail.com>
2024-06-01 21:15:14 -0600

    impresión de error de la validación de campos de nuevo usuario

--- src/middlewares/userDataPostMiddleware.ts
+++ src/middlewares/userDataPostMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { check, validationResult } from 'express-validator';
 import AppError from '../core/errors/AppError';
+import Logger from '../utils/Logger';
 
 export const userFieldsRequired = [
     check('name').notEmpty().isLength({ max: 50 }).trim().escape(),
@@ -10,10 +11,13 @@ export const userFieldsRequired = [
 ];
 
 
-export const userDataPostMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const userDataPostMiddleware = (
+    req: Request, _: Response, next: NextFunction
+) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty()) {
+        Logger.error(`Error: ${JSON.stringify(errors.array())}`);
         throw new AppError('There are fields that are required.', 400);
     }
 
