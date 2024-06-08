efb93f741746360105a8cd07f5d84309527a7e5d
Josh <josh@imac.com>
2024-06-08 11:16:14 -0600

    comentarios

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -7,15 +7,25 @@ import {
 } from '../config/env';
 import { decrypt, encrypt } from './cryptoUtils';
 
-
-const jwtValuesEnv = (isRefreshToken: boolean): { jwtSecret: string, jwtExpiresIn: string } => {
+/**
+ * En base si el token es para refrescar o validar se retorna el valode del
+ * secret
+ */
+const jwtValuesEnv = (
+    isRefreshToken: boolean
+): { jwtSecret: string, jwtExpiresIn: string } => {
     return {
         jwtSecret: isRefreshToken ? JWT_REFRESH_SECRET : JWT_SECRET,
         jwtExpiresIn: isRefreshToken ? JWT_REFRESH_EXPIRES_IN : JWT_EXPIRATION
     };
 }
 
-export const generateToken = (userId: number, isRefreshToken = false): string => {
+/**
+ * Generación de un nuevo JWT token (authenticar/refrescar)
+ */
+export const generateToken = (
+    userId: number, isRefreshToken = false
+): string => {
     const { jwtSecret, jwtExpiresIn } = jwtValuesEnv(isRefreshToken);
 
     const encryptedId = encrypt(`${userId}`);
@@ -23,6 +33,9 @@ export const generateToken = (userId: number, isRefreshToken = false): string =>
     return jwt.sign(tokenPayload, jwtSecret, { expiresIn: jwtExpiresIn });
 };
 
+/**
+ * Verificación y decodificación de JWT token
+ */
 export const verifyToken = (token: string, isRefreshToken = false): number => {
     const { jwtSecret } = jwtValuesEnv(isRefreshToken);
 
