14d74e9ab874d07340366b2ad6e75f93bbf435d4
Josh <josh@imac.com>
2024-06-09 16:52:30 -0600

    se elimina las librerias de tsoa para el swagger

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-import { Route } from 'tsoa';
 import { IUserController } from '../core/interfaces/user/IUserController';
 
-@Route("users")
 class UserController implements IUserController {
     async info(_: Request, res: Response, next: NextFunction): Promise<void> {
         try {
