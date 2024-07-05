038bda7d76e12830ba1f831e32d09a0e5df0e4b2
Josh <josh@imac.com>
2024-07-05 14:04:02 -0600

    se elimina la url de dev

--- src/config/swaggerConfig.ts
+++ src/config/swaggerConfig.ts
@@ -17,10 +17,6 @@ const options: swaggerJsdoc.Options = {
                 url: "http://localhost:8080",
                 description: "Local server"
             },
-            {
-                url: "https://miscuentas-app-user-service.onrender.com",
-                description: "Dev server"
-            },
         ],
     },
     apis: ['./src/routes/*.ts'],
